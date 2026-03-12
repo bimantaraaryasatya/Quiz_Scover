e451f3cf5a4118c6c6899c7c4cc64c94f85426f5
bimantaraaryasatya <bimantaraaryasatya@gmail.com>
2026-03-12 19:10:24 +0700

    Revert "test push"
    This reverts commit 27a95bdfc17749a1c5edcfbbc705adfe6d8a3088.

--- app/admin/history/page.tsx
+++ app/admin/history/page.tsx
@@ -107,7 +107,7 @@ export default function AdminUser() {
                         <tr key={item} className="hover:bg-gray-50 transition-colors">
                             <td className="px-6 py-4">00001</td>
                             <td className="px-6 py-4">
-                            Christine Brookss
+                            Christine Brooks
                             </td>
                             <td className="px-6 py-4">
                             christine@gmail.com
